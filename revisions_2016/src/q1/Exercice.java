29f5845cb2fe3ffac0be1c69703bca0809a280a1
nikotsi <nikolaon.tsiras@gmail.com>
2016-06-02 10:18:21 +0200

    1

--- revisions_2016/src/q1/Exercice.java
+++ revisions_2016/src/q1/Exercice.java
@@ -1,5 +1,39 @@
 package q1;
 
 public class Exercice {
-
+	private int dificultyLevel;
+	private String enoncé;
+	private int typeExerice;
+	final int TYPE_EXERCICE_QCM = 1;
+	final int TYPE_EXERCICE_A_TROU = 2;
+	final int TYPE_EXERCICE_VRAI_OU_FAUX = 3;
+	final int TYPE_EXERCICE_AUTRE = 4;
+	public Exercice() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Exercice(int dificultyLevel, String enoncé, int typeExerice) {
+		super();
+		this.dificultyLevel = dificultyLevel;
+		this.enoncé = enoncé;
+		this.typeExerice = typeExerice;
+	}
+	public int getDificultyLevel() {
+		return dificultyLevel;
+	}
+	public void setDificultyLevel(int dificultyLevel) {
+		this.dificultyLevel = dificultyLevel;
+	}
+	public String getEnoncé() {
+		return enoncé;
+	}
+	public void setEnoncé(String enoncé) {
+		this.enoncé = enoncé;
+	}
+	public int getTypeExerice() {
+		return typeExerice;
+	}
+	public void setTypeExerice(int typeExerice) {
+		this.typeExerice = typeExerice;
+	}
 }
